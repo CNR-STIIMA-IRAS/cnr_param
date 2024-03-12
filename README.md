93040660541925906c29a8745052d36c70106b6b
Cesare Tonola <c.tonola001@unibs.it>
2024-03-12 15:32:52 +0100

    default folder set to /tmp/cnr_param and basic readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# cnr_param
+
+A package to read and write parameters for your C++ application.
+
+## Getting Started
+The basic way to load parameters is using this command from terminal
+```
+cnr_param_server -p path-to-file
+```
+Get help typing
+```
+cnr_param_server -h
+```
+By default, the parameters are saved in '/tmp/cnr_param'. You can choose another folder by setting in the 'bashrc' file 
+```
+export CNR_PARAM_ROOT_DIRECTORY="your directory"
+```
+TODO
--- src/cnr_param_server/param_server.cpp
+++ src/cnr_param_server/param_server.cpp
@@ -5,7 +5,8 @@
 #include <string>
 #include <iostream>
 
-#include <boost/interprocess/detail/os_file_functions.hpp>
+//#include <boost/interprocess/detail/os_file_functions.hpp>
+#include<boost/filesystem/path.hpp>
 
 #include <cnr_param_server/utils/args_parser.h>
 #include <cnr_param_server/utils/yaml_manager.h>
@@ -13,30 +14,46 @@
 
 int main(int argc, char* argv[])
 {
+  const std::string default_shmem_name = "param_server_default_shmem";
+  const std::string default_param_root_directory = "/tmp/cnr_param";
 
-   const std::string default_shmem_name = "param_server_default_shmem";
-   const std::string default_param_root_directory = boost::interprocess::ipcdetail::get_temporary_path();
-   
-   std::string param_root_directory;
-   const char* env_p = std::getenv("CNR_PARAM_ROOT_DIRECTORY");
-   param_root_directory = (env_p) ? std::string(env_p) : default_param_root_directory;
+  std::string param_root_directory;
+  const char* env_p = std::getenv("CNR_PARAM_ROOT_DIRECTORY");
 
-   int rc = setenv("CNR_PARAM_ROOT_DIRECTORY", param_root_directory.c_str(), true);
-   if(rc!=0)
-   {
-      std::cerr << "Errono" << rc << ": " << strerror(rc) << std::endl;
-   }
+  if(env_p)
+  {
+    param_root_directory = std::string(env_p);
+  }
+  else
+  {
+    // Check if default_param_root_directory exists; if not, create it
+    boost::filesystem::path dir(default_param_root_directory);
+    if (not boost::filesystem::exists(dir))
+    {
+      if (not boost::filesystem::create_directories(dir)) {
+        std::cerr << "Failed to create directory: " << default_param_root_directory << std::endl;
+        return 1; // or handle error appropriately
+      }
+    }
+    param_root_directory = default_param_root_directory;
+  }
 
-   // Parsing of program inputs
-   ArgParser args(argc, argv, default_shmem_name);
+  int rc = setenv("CNR_PARAM_ROOT_DIRECTORY", param_root_directory.c_str(), true);
+  if(rc!=0)
+  {
+    std::cerr << "Errono" << rc << ": " << strerror(rc) << std::endl;
+  }
 
-   // Parsing of the file contents, and storing in YAML::Node root
-   YAMLParser yaml_parser(args.getNamespacesMap());
+  // Parsing of program inputs
+  ArgParser args(argc, argv, default_shmem_name);
 
-   // Streaming of all the files in mapping_files: shared memes mapped on files
-   // The tree is build under the 'param_root_directory'
-   YAMLStreamer yaml_streamer(yaml_parser.root(), param_root_directory);
+  // Parsing of the file contents, and storing in YAML::Node root
+  YAMLParser yaml_parser(args.getNamespacesMap());
 
-   // Done!
+  // Streaming of all the files in mapping_files: shared memes mapped on files
+  // The tree is build under the 'param_root_directory'
+  YAMLStreamer yaml_streamer(yaml_parser.root(), param_root_directory);
+
+  // Done!
   return 0;
 }
