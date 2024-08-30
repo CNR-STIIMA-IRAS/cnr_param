36f5d1bee5ca1fd0adf5f18f2124f35da7164cd5
Cesare Tonola <c.tonola001@unibs.it>
2024-08-30 10:27:37 +0200

    IMPORTED_LOCATION_NOCONFIG /libcnr_param generates compilation errors, commented out

---
+++ CMakeFiles/cnr_param.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cnr_param.dir/src/cnr_param/cnr_param.cpp.o"
+  "libcnr_param.pdb"
+  "libcnr_param.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cnr_param.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/cnr_param_core.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/riemann/projects/cari_motion_planning_ws/cnr_param/src/cnr_param/core/colors.cpp" "/home/riemann/projects/cari_motion_planning_ws/cnr_param/CMakeFiles/cnr_param_core.dir/src/cnr_param/core/colors.cpp.o"
+  "/home/riemann/projects/cari_motion_planning_ws/cnr_param/src/cnr_param/core/filesystem.cpp" "/home/riemann/projects/cari_motion_planning_ws/cnr_param/CMakeFiles/cnr_param_core.dir/src/cnr_param/core/filesystem.cpp.o"
+  "/home/riemann/projects/cari_motion_planning_ws/cnr_param/src/cnr_param/core/string.cpp" "/home/riemann/projects/cari_motion_planning_ws/cnr_param/CMakeFiles/cnr_param_core.dir/src/cnr_param/core/string.cpp.o"
+  "/home/riemann/projects/cari_motion_planning_ws/cnr_param/src/cnr_param/core/yaml.cpp" "/home/riemann/projects/cari_motion_planning_ws/cnr_param/CMakeFiles/cnr_param_core.dir/src/cnr_param/core/yaml.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "BOOST_ALL_NO_LIB"
+  "BOOST_FILESYSTEM_DYN_LINK"
+  "BOOST_SYSTEM_DYN_LINK"
+  "cnr_param_core_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "include"
+  "/usr/include/eigen3"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
