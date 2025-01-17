744ebe3c6d129120b4efff7f5fa082fb45dd4c7b
Nicola Pedrocchi <nicola.pedrocchi@stiima.cnr.it>
2025-01-17 10:39:15 +0100

    stand alone compile OK

--- cmake/cnr_param-compile-options.cmake
+++ cmake/cnr_param-compile-options.cmake
@@ -1,5 +1,5 @@
-set(cnr_param_BUILD_AS_A_CATKIN_PACKAGE  ON )
+set(cnr_param_BUILD_AS_A_CATKIN_PACKAGE  OFF )
 set(cnr_param_BUILD_AS_AN_AMENT_PACKAGE  OFF )
-set(cnr_param_COMPILE_ROS1_MODULE        ON )
+set(cnr_param_COMPILE_ROS1_MODULE        OFF )
 set(cnr_param_COMPILE_ROS2_MODULE        OFF )
 set(cnr_param_COMPILE_MAPPED_FILE_MODULE ON )
